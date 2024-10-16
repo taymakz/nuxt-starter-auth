253cc3171d4a85734e6c54ad88eab43812096186
TaymaZ <taymazak1382@gmail.com>
2024-10-16 20:53:56 +0330

    feat: constans, main Title

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,5 +1,5 @@
-export const appName = 'تیمرس'
-export const appTitle = 'فروشگاه ایننترنتی تیمرس'
-export const appDescription = `هر آنچه که نیاز دارید با بهترین قیمت از ${appName} بخرید! جدیدترین انواع گوشی موبایل، لپ تاپ، لباس، لوازم آرایشی و بهداشتی، کتاب، لوازم خانگی، خودرو و... با امکان تعویض و مرجوعی آسان | ✓پرداخت در محل ✓ضمانت بازگشت کالا - برای خرید کلیک کنید!`
-export const appNameEn = 'Timmerce'
+export const appName = 'Starter'
+export const appTitle = 'Starter'
+export const appDescription = 'app description'
+export const appNameEn = 'Starter'
 export const appDesktopStartMinWidth = '(min-width: 1024px)'
